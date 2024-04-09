51778e2ed48214362805b6584ede51687347f1b1
Ujun <jujunjuna189@gmail.com>
2024-04-09 14:42:17 +0700

    Update

---
+++ database/migrations/2024_04_09_143409_create_dt_ranpur.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDtRanpur extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dt_ranpur', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('jenis')->nullable();
+            $table->string('nomor')->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dt_ranpur');
+    }
+}
