2bdf22c4187f1c360222226a84bd2f2bba8b2abf
Ujun Junaedi <jujunjuna189@gmail.com>
2026-04-17 08:33:00 +0700

    update

--- resources/views/artikel/view.blade.php
+++ resources/views/artikel/view.blade.php
@@ -2,7 +2,27 @@
 @section('content')
 <div class="container py-4">
     <div class="w-100">
-        {!! json_decode($artikel->artikel) !!}
+        @php
+            $artikelContent = $artikel->artikel;
+            $output = $artikelContent;
+            
+            // Cek apakah string adalah valid JSON
+            $firstChar = trim($artikelContent)[0] ?? '';
+            if ($firstChar === '{' || $firstChar === '[' || $firstChar === '"') {
+                $decoded = json_decode($artikelContent, true);
+                
+                if (json_last_error() === JSON_ERROR_NONE && $decoded !== null) {
+                    // Jika hasil decode adalah string, gunakan string tersebut
+                    if (is_string($decoded)) {
+                        $output = $decoded;
+                    } else {
+                        // Jika object/array, convert kembali ke string
+                        $output = json_decode($artikelContent);
+                    }
+                }
+            }
+        @endphp
+        {!! $output !!}
     </div>
 </div>
 @endsection
